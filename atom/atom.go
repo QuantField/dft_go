7aeb4dad46c8e569d2392e3023dca1d71a985869
Kamel Saadi <saadi.kamel@gmail.com>
2020-09-14 22:41:37 +0100

    few changes

--- atom/atom.go
+++ atom/atom.go
@@ -1,86 +1,62 @@
 package atom
 
+import (
+	
+	sol "quantum/sol"
+
+)
+
 //import "fmt"
 
-//============================= Radius =============================
-
-// INFINITY represents infinity at the center r=0
-// This is set in order to work with Potentials at r=0
-// All infinite values are set to 0 at r=0, it doesn't affect the solution
-// as we work with r*psi(r) which = 0 anyway
-const INFINITY = 0.0 
-
-// Radius reprsent r values from 0 to Rmax with step h and N+1 points
-type Radius struct {
-	Rmax  float64
-	N     int
-	GridStep   float64
-	Array []float64
-}
-
-// NewRadius Radius constructor
-func NewRadius(Rmax float64, N int) *Radius {
-
-	dr := Rmax / float64(N)
-	rad := make([]float64, N+1)
-	for i := 0; i < len(rad); i++ {
-		rad[i] = float64(i) * dr
-	}
-
-	r := Radius{
-		Rmax:  Rmax,
-		N:     N,
-		GridStep:     dr,
-		Array: rad,
-	}
-	return &r
-}
 
 
 //============================= Atom =============================
 
 // Atom stuctue
 type Atom struct {
-	Z            float64
-	r            []float64
+	Z            float64	
 	Vcoulomb     []float64
 	Vcentrifugal []float64
 	// Total Potential, Veff = Veff0 + Vcentrifugal
 	Veff0 []float64 // Veff0 place holer for varying  Vks
 	Veff  []float64
+	grid *sol.LogGrid
 }
 
 // NewAtom constructor
-func NewAtom(Z float64, radius *Radius) *Atom {
+func NewAtom(Z float64, grid *sol.LogGrid) *Atom {
 	// r0 := make([]float64, len(r))
 	// copy(r0, r) //deep copy
-	r := radius.Array
+	
 	a := Atom{
 		Z: Z,
-		r: r,
+		grid: grid,
 	}
-	a.Vcoulomb = make([]float64, len(r))
-	a.Vcentrifugal = make([]float64, len(r))
-	a.Veff0 = make([]float64, len(r))
-	a.Veff = make([]float64, len(r))
+	n := len(grid.Val)
+	a.Vcoulomb = make([]float64, n)
+	a.Vcentrifugal = make([]float64, n)
+	a.Veff0 = make([]float64, n)
+	a.Veff = make([]float64, n)
 	return &a
 }
 
 // SetVcoulomb  Sets the Coulomb Potential -Z/r
 func (a *Atom) SetVcoulomb() {
-	for i := 1; i < len(a.r); i++ {
-		a.Vcoulomb[i] = -a.Z / a.r[i]
+	r := a.grid.Val 
+	for i := 1; i < len(r); i++ {
+		a.Vcoulomb[i] = -a.Z /r[i]
 	}
-	a.Vcoulomb[0] = INFINITY
+	a.Vcoulomb[0] = 0 //INFINITY
 }
 
 // SetVcentrifugal Sets the centrifugal  Potential 0.5l(l+1)/r^2
 func (a *Atom) SetVcentrifugal(j int) {
 	l := float64(j)
-	for i := 1; i < len(a.r); i++ {
-		a.Vcentrifugal[i] = 0.5 * l * (l + 1) / (a.r[i] * a.r[i])
+	r2 := a.grid.ValSqr
+	for i := 1; i < len(r2); i++ {
+		a.Vcentrifugal[i] = 0.5*l*(l + 1)/r2[i]
 	}
-	a.Vcentrifugal[0] = INFINITY
+	a.Vcentrifugal[0] = 0 //INFINITY
 }
 
 // SetVeff Sets effective potential Veff = Veff0 + 0.5l(l+1)/r^2
@@ -89,10 +65,11 @@ func (a *Atom) SetVcentrifugal(j int) {
 func (a *Atom) SetVeff(Veff0 []float64) {
 	var V []float64
 	V = Veff0
+	r := a.grid.Val
 	if Veff0 == nil {
 		V = a.Vcoulomb
 	}
-	for i := 0; i < len(a.r); i++ {
+	for i := 0; i < len(r); i++ {
 		a.Veff[i] = V[i] + a.Vcentrifugal[i]
 	}
 }
--- main.go
+++ main.go
@@ -2,49 +2,59 @@ package main
 
 import (
 	"fmt"
-	 atom "quantum/atom"
-	 solEng  "quantum/sol"
+	"math"
+	atom "quantum/atom"
+	sol "quantum/sol"
 )
+
+
+
 
 func main() {
 
-	// defining the ratius with max length of 10 units
-	// grid of N+1 points
-	Rmax := 10.0
-	N := 4000
-	radius := atom.NewRadius(Rmax, N)
+	N := 1000
+	// 10^-5..10^1
+	grid := sol.NewLogGrid(-5,1, N)
+	// fmt.Println(grid.Val)
+	// fmt.Println(grid.LogVal)
+	// fmt.Println(grid.ValSqr )
+	// fmt.Println(grid.ValSqrRoot)
+
+
+	
+	
 
 	// Atom Z = 1
-	H := atom.NewAtom(1, radius)
+	H := atom.NewAtom(1, grid)
 	H.SetVcoulomb()
 	H.SetVcentrifugal(0) // l=0
 	H.SetVeff(nil)
 
-	sol := solEng.NewSolution(radius, H.Veff)
+	sol := sol.NewSolution(radius, H.Veff)
 
 	fmt.Println("Solution: ")
 
 	q := sol.FindSolutionIntervals(-10, -0.0001, .01)
-	
-	for key, interval:=range q {
-		E, _ := sol.FindEigenState(interval.Lower,  interval.Upper)
+
+	for key, interval := range q {
+		E, _ := sol.FindEigenState(interval.Lower, interval.Upper)
 		fmt.Println("n = ", key, " Solution interval", interval, "E =", E)
-	} 
+	}
 	//fmt.Println(q)
-	/* 
-	E, psi := sol.FindEigenState(-1, 0)
-	if psi != nil {
-		fmt.Println("E = ", E)
-		fmt.Println("Psi = ", psi[0:2])
-		fmt.Println("Psi = ", psi[len(psi)-2:])
+	/*
+		E, psi := sol.FindEigenState(-1, 0)
+		if psi != nil {
+			fmt.Println("E = ", E)
+			fmt.Println("Psi = ", psi[0:2])
+			fmt.Println("Psi = ", psi[len(psi)-2:])
 
-		u := solEng.NewEigenEigenState(E, 1 ,0, psi,  sol.GridStep)
-		
-		solEng.SaveData(sol.R, u.Value, "misc/psi.dat")
-		solEng.SaveData(sol.R, u.ProbabilityDensity(), "misc/dens.dat")
-		solEng.SaveData(sol.R, sol.V, "misc/pot.dat")
-		fmt.Println(solEng.Integrate(u.ProbabilityDensity(), sol.GridStep ) )
-	 }
-	 */
+			u := solEng.NewEigenEigenState(E, 1 ,0, psi,  sol.GridStep)
 
+			solEng.SaveData(sol.R, u.Value, "misc/psi.dat")
+			solEng.SaveData(sol.R, u.ProbabilityDensity(), "misc/dens.dat")
+			solEng.SaveData(sol.R, sol.V, "misc/pot.dat")
+			fmt.Println(solEng.Integrate(u.ProbabilityDensity(), sol.GridStep ) )
+		 }
+	*/
+ 
 }
